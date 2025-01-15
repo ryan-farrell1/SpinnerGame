75a28fcee6c1c7b1fa7fad11b94ba4ca82da192c
ryan-farrell1 <156547687+ryan-farrell1@users.noreply.github.com>
2025-01-15 15:41:13 -0500

    Add files via upload
    New Attempt 2

--- spinners.py
+++ spinners.py
@@ -238,7 +238,8 @@ class MassTester():
                           xaxis_title='Game Length',
                           yaxis_title='Frequency',
                           height=500,
-                          xaxis=dict(range=[shortest_game - plot_buffer, longest_game + plot_buffer], autorange=False)
+                          xaxis=dict(range=[shortest_game - plot_buffer, longest_game + plot_buffer], autorange=False),
+                          xbins=dict(start=shortest_game - plot_buffer, end=longest_game + plot_buffer, size=1)
                         )
         
         # Return Figure and Game Statistics
